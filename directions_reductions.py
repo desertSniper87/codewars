e9cf0092a5b7dc0f2a90f73c12f9d946588137d9
desertSniper87 <torsho@localhost.localdomain>
2018-01-27 16:35:28 +0600

    succ kata

--- directions_reductions.py
+++ directions_reductions.py
@@ -1,4 +1,8 @@
+from random import randrange
+from random import choice
+
 def dirReduc(arr):
+    print(arr)
     #TODO Make custom test cases and then submit
     back = {
             "NORTH": "SOUTH",
@@ -7,17 +11,37 @@ def dirReduc(arr):
             "WEST" : "EAST"
            }
     result = []
-    for i in a:
+    for i in arr:
+        print("result, i, back[i]", result, i, back[i])
         if len(result)!=0 and result[-1]==back[i]:
-            result.remove(back[i])
+            del result[-1]
         else:
             result.append(i)
-    # if len(result)==2:
-        # return [result[1], result[0]]
+
     return result
 
 if __name__ == "__main__":
     # a = ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]
     a=["NORTH", "WEST", "SOUTH", "EAST"]
-    x = dirReduc(a)
-    print(x)
+    bug = ['NORTH', 'EAST', 'NORTH', 'EAST', 'WEST', 'SOUTH']
+
+    # directions=["NORTH", "WEST", "SOUTH", "EAST"]
+    # https://stackoverflow.com/questions/306400/how-to-randomly-select-an-item-from-a-list?rq=1
+    # for _ in range(999999999):
+    # while(True):
+        # p = []
+        # x = []
+        # for i in range(randrange(20)):                             
+            # p.append(choice(directions))
+        # x = dirReduc(p)
+        # print("~"*120)
+        # print(" "*120)
+        # print(p, '\n', x)
+        # if x==['NORTH', 'NORTH', 'EAST', 'SOUTH']:
+            # break
+        # print(" "*120)
+        # print("~"*120)
+        
+
+
+    print(dirReduc(bug))
