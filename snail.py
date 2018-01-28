ce43e3006f26e0e51393bb0a07a1b42d1ef84ce1
desertSniper87 <torsho@localhost.localdomain>
2018-01-29 00:15:37 +0600

    update gitignore

--- snail.py
+++ snail.py
@@ -9,10 +9,49 @@
 # def flatten(listOfLists):
     # "Flatten one level of nesting"
     # return chain.from_iterable(listOfLists)
+import math
+
+def s1(array):
+    """TODO: Docstring for s1.
+
+    :array: TODO
+    :returns: TODO
+
+    """
+    dim = int(math.sqrt(len(array)))
+    # print(dim)
+    result = array[0:dim]
+    # print(array)
+    # print(result)
+    # print("array, array[dim:]", array, array[dim:])
+    # for index_i, i in enumerate(array[dim:]):
+    for index_i, i in enumerate(array[dim:], start=dim):
+        print("i ,index_i",i ,index_i)
+        if (index_i%dim == 0):
+            result.append(i)
+            # print("array[index_i], i, index_i", array[index_i], i, index_i)
+            # del array[index_i]
+            # print("array", array)
+            # print(result)
+    # array = array[dim:]
+    # print(array)
+    print(result)
+    return result, array
+        # if ()
+
 
 def snail(array):
     # l = flatten(array)
-    flat = [x for sublist in nested for x in sublist]
+    flat = [x for sublist in array for x in sublist]
+    # print(flat)
+    result = []
+    while(flat!=[]):
+        r, flat = s1(flat)
+        # print(flat)
+        result.append(r)
+        # print(result)
+
+        
     # result = []
     # for a in array:
         # # for index_i, i in enumerate(a):
