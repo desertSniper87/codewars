71e25356add6f47b1d6f12a750313414f5474b61
desertsniper87 <torshobuet@gmail.com>
2019-04-24 17:49:47 +0600

    dna-to-rna

--- cpp/dna-to-rna-conv.cpp
+++ cpp/dna-to-rna-conv.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 
 std::string DNAtoRNA(std::string dna){
-  // your code here
-    std::cout<< dna; 
-    return dna;
+    int dna_size = dna.length();
+    char *rna = new char[dna_size];
+
+
+    for (int i = 0; i < dna_size; ++i) {
+        std::cout<< dna[i];
+        if (dna[i] == 'T')
+            rna[i] = 'U';
+        else
+            rna[i] = dna[i];
+    }
+    return rna;
 }
