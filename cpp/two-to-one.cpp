e5f165103146c2db5b3b96c8b8b91f69184cb339
desertsniper87 <torshobuet@gmail.com>
2019-04-24 22:47:16 +0600

    two to one

--- cpp/two-to-one.cpp
+++ cpp/two-to-one.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
+#include <vector>
 
-class TwoToOne
-{ public:
+class TwoToOne {
+public:
     static std::string longest(const std::string &s1, const std::string &s2);
 };
 
 std::string TwoToOne::longest(const std::string &s1, const std::string &s2) {
-    std::cout<< s1;
-    std::cout<< s2;
+    std::vector<char> v;
 
-    return nullptr;
+    for (int i = 0; i < s1.length(); ++i) {
+        v.push_back(s1[i]);
+    }
+
+    for (int i = 0; i < s2.length(); ++i) {
+        v.push_back(s2[i]);
+    }
+
+
+    sort(v.begin(), v.end());
+
+    std::vector<char>::iterator it;
+    it = std::unique(v.begin(), v.end());
+
+    v.erase(it, v.end());
+
+    std::string str(v.begin(), v.end());
+
+    return str;
 }
