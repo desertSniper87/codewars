65f4f042e25cf6baf53e92f199af28db0a2a9910
desertsniper87 <torshobuet@gmail.com>
2019-04-25 16:47:50 +0600

    consecutive string done

--- cpp/consecutive-strings-test.cpp
+++ cpp/consecutive-strings-test.cpp
@@ -17,10 +17,63 @@ void dotest(std::vector<std::string> arr, int k, std::string expected) {
 Describe(longestConsec_Tests) {
     It(Fixed__Tests)
     {
-        std::vector<std::string> arr = {"zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"};
+        std::vector<std::string> arr;
+
+        arr = {"zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"};
         dotest(arr, 2, "abigailtheta");
+
         arr = {"ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"};
         dotest(arr, 1, "oocccffuucccjjjkkkjyyyeehh");
+
+        arr = {};
+        dotest(arr, 1, "");
+
+        arr = {"itvayloxrp", "wkppqsztdkmvcuwvereiupccauycnjutlv", "vweqilsfytihvrzlaodfixoyxvyuyvgpck" };
+        dotest(arr, 2, "wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck");
+
+        arr = {"it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz"};
+        dotest(arr, 15, "");
+
+//        arr = {"NJIgbfGjI", "DihcFaEXc", "rqDfQjHflKMpBvWsuLAeXUNl",
+//               "pAsYNRfWzpOkPPzGcZccc", "sYwtcVwm", "DlXYJaFpzuPCKD",
+//               "dEqCCaJiIEtGkBQUjjchcPVu", "hwUvaKMbVMBCHEO", "PVZjWNWlxLdtV",
+//               "zFwnejrnjpxvsraxFoVnra", "nESoOKfPzyvfuodZPZ", "IABmeezq",
+//               "UeUjwQerhxPTc", "NrczdyVtYbEFgKNsRtlcVYPdl",
+//               "WxQOjSzIlBAXFCSeHwlH", "YYjdgT", "veaasDKMYVKClywcmTlCY",
+//               "iYYWdowCUHfkexitR", "xZoDpcwJLgb", "egKXwHH",
+//               "CeMcRvbsjPBPHMkUPsJ", "pUQnDTpqGAYfhaMbNE", "nxcntCcmgUmwULr",
+//               "KOUKiHsBuLSRDg", "tquNfHnPPMbUT", "LGAuuCyOu", "mlDAImCvfRbR",
+//               "cMFbpWCCmQDgNPl", "LOxVeqTZZKjysXhalWmyE", "kDljX",
+//               "OGCWmgbfCjPqkNtou", "LsHqntlMcLddaOJ",
+//               "mFmmkXWnIiSGjQHmQdQZmva", "pehZeQqGKc", "deJDq", "eCtnWcvsH",
+//               "YTYBeqZcxcUDmZuxKzW", "qbqvfKPDDNBxkJYpYWbYlkeDK",
+//               "NNnKcJwXCaWwiSaBg", "QXYxfMQJEMkLWleznpioNwaI"};
+//        dotest(arr, 6, "zFwnejrnjpxvsraxFoVnranESoOKfPzyvfuodZPZIABmeezqUeUjwQerhxPTcNrczdyVtYbEFgKNsRtlcVYPdlWxQOjSzIlBAXFCSeHwlH");
+
+        arr = {"zJHRMo", "YZVFKMLsTspcFoBYyCKDf", "BvMMsewgWtAOoE",
+            "MexOYiTUqdTvKoGuDlZamkiA", "EyALdiAfdvdNYceTwvKwn",
+            "wmtTiyIWwlnhJfvoh", "mobVtCoMRxVgjZLHxctaE", "swwbmNOzyqjeE",
+            "qMaUuD", "WEXfZG", "bLBKqOWlqeNykeXDFWDlkcWR",
+            "aaaxQmXgPLtaFVAwrpgzt", "mxjuUZPqUUIdqnJyznEEl", "WExTKDrp",
+            "AXYBjjgmxi", "fJaYay", "XdBsTJ", "XXdpgozG", "VAiaOyFURDABp",
+            "JyKQLc", "MqLXTJBXQsetjlogiLh", "fHczQo", "lzacAiB", "llrTXtPt",
+            "bkwvIMYiEzUY", "FDfcjMqE"};
+        dotest(arr, 1, "MexOYiTUqdTvKoGuDlZamkiA");
+
+        arr = {"uNATikZuSBNSxaHLubuOg", "jOuWzzvYeRKNrrPiRrLfB",
+            "kNbQFwgWpGtXzyWpPeGoVqxj", "dhKRSSobVLeFOMTk",
+            "ctgMvOLZFXWlvyrbAuIB", "SBnenPDsCr", "WhUwPhLoPVhONZgxQLfjgGUm",
+            "RVzwZLBNhYluuGvPOewTN", "XkTxgOXcktNKNKvPKb", "rxdXLFteTytVKRC",
+            "bFUWxeJpyWsMc", "libFErIfOBYROR", "ydwjZfCZUKxjBXJU",
+            "iCmUBqMkORQg", "jWLzbHmMSyTGxHGH", "xLnHkVPDZHAocvnW",
+            "pXeCuzMSUzhevLucgmFBEoG", "LWjuuALQriknIN",
+            "GQsBQqCBMLJPDVgshoWzXBtkt", "vjYFoPDzleQfwW",
+            "XiDhALCXveyrOLIetwEmHIDG", "baBscpjsqacjDUtMgyLRzTFMc", "JRziP",
+            "keQgGgOnILaQkrcgvVXN", "jGWDtjyXFbqgXLeLNvzlo", "PVoBUKBmtWBXApD",
+            "tWArSntoYylDSFsSpgKxMvRa", "hcGsra", "nKavRNXgDCWpzJqUsR",
+            "EGLpIpOnJ", "ntQTiGrkicHvNJEoiferJoprn"};
+
+        dotest(arr, 1, "GQsBQqCBMLJPDVgshoWzXBtkt");
     }
 };
 
--- cpp/consecutive-strings.cpp
+++ cpp/consecutive-strings.cpp
@@ -6,32 +6,67 @@
 class LongestConsec {
 public:
     static std::string longestConsec(std::vector<std::string> &strarr, int k);
-    static bool compareLen(const std::string& a, const std::string& b) {
+    static bool compareLenReverse(const std::string &a, const std::string &b) {
         return (a.size() > b.size());
+    }
+//    static bool compareAlpha(const std::string& a, const std::string& b) {
+//        return (a < b);
+//    }
+    static bool compareLen(const std::string &a, const std::string &b) {
+        return (a.size() < b.size());
     }
 };
 
 std::string LongestConsec::longestConsec(std::vector<std::string> &strarr, int k) {
-/*    for (const auto &item : strarr) {
-        std::cout<< item.length()<< std::endl;
-    }*/
-    std::vector<std::string> &array = strarr;
+    std::vector<std::string> &string_container = strarr;
     std::vector<std::string> concat_string_container;
     std::string cat_string;
-    for (int i = 0; i < array.size()-1; ++i) {
-//        std::cout<< array[i]<< ' '<< array[i].length()<< ' '<< array[i+1]<< array[i+1].length()<< std::endl;
-        cat_string = array[i] + array[i + 1];
-        concat_string_container.push_back(cat_string);
+
+    int size_of_vector = string_container.size();
+
+    if (k>size_of_vector){
+        return "";
     }
 
-//    std::cout<< k;
-    std::sort(concat_string_container.begin(), concat_string_container.end(), compareLen);
+    std::cout<< std::endl<< "************  "<< k<< "  **************"<< std::endl;
 
-    for (const auto &item : concat_string_container) {
-        std::cout<< item<< std::endl;
+    for (const auto &item : string_container) {
+        std::cout<< item<< ' ';
     }
 
-    return concat_string_container.front();;
+    std::cout<< std::endl;
+
+    if (string_container.empty() == 1){
+        return "";
+    }
+
+    if (k == 1){
+        std::string max_elem = *std::max_element(string_container.begin(), string_container.end(), compareLen);
+        std::cout<< max_elem<< ' '<< max_elem.length();
+
+        for (const auto &item : string_container) {
+            if (item.length() == max_elem.length())
+                return item;
+        }
+    } else {
+        for (int i = 0; i < size_of_vector-k+1; ++i) {
+            cat_string = "";
+            for (int j = i; j<k+i; ++j) {
+                cat_string += string_container[j];
+            }
+            concat_string_container.push_back(cat_string);
+        }
+
+        std::string max_elem = *std::max_element(concat_string_container.begin(), concat_string_container.end(),
+                                                 compareLen);
+
+        std::cout<< max_elem<< ' '<< max_elem.length();
+
+        for (const auto &item : concat_string_container) {
+            if (item.length() == max_elem.length())
+                return item;
+        }
+    }
 }
 
 
