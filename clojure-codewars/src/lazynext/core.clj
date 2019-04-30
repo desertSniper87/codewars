9ec465c794672f9de247712d08831be3c056a903
desertsniper87 <torshobuet@gmail.com>
2019-04-30 21:45:46 +0600

    lazy next complete

--- clojure-codewars/src/lazynext/core.clj
+++ clojure-codewars/src/lazynext/core.clj
@@ -1,16 +1,24 @@
 (ns lazynext.core
-  (:import (clojure.lang LongRange PersistentVector)))
+  (:import (clojure.lang LongRange PersistentVector PersistentList)))
 
 (defn next-item
   "Returns the value that comes after item in xs or nil"
   [xs item]
+  (println (class xs))
+  (println xs)
+  (println item)
   (cond
     (= (class xs) LongRange)
     (nth xs (+ (.indexOf xs item) 1))
     (= (class xs) String)
-    (get xs (+ (clojure.string/index-of xs item) 1))
+    (cond
+      (nil? (clojure.string/index-of xs item)) nil
+      :else
+     (get xs (+ (clojure.string/index-of xs item) 1)))
     (= (class xs) PersistentVector)
     (cond
       (< (.indexOf xs item) 0) nil
       :else
-      (get xs (+ (.indexOf xs item) 1)))))
+      (get xs (+ (.indexOf xs item) 1)))
+    (= (class xs) PersistentList)
+    (nth xs (+ (.indexOf xs item) 1))))
--- clojure-codewars/test/lazynext/test.clj
+++ clojure-codewars/test/lazynext/test.clj
@@ -3,7 +3,10 @@
             [lazynext.core :refer [next-item]]))
 
 (deftest SampleTests
-  ;(is (= (next-item (range 1 25) 12) 13))
-  ;(is (= (next-item "testing" \t) \e))
-  (is (nil? (next-item [:a :b :c] :d))))
-  ;(is (nil? (next-item [:a :b :c] :c))))
+  (is (= (next-item (range 1 25) 12) 13))
+  (is (= (next-item "testing" \t) \e))
+  (is (nil? (next-item "testing" \k)))
+  (is (nil? (next-item [:a :b :c] :d)))
+  (is (nil? (next-item [:a :b :c] :c)))
+  (is (nil? (next-item (list :a :b :c) :d)))
+  (is (= (next-item (list :a :b :c :d) :a) :b)))
