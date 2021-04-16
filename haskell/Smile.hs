5bec1fa644c92b3e3e22cd66af93f2b810d43d5f
desertsniper87 <torshobuet@gmail.com>
2021-04-17 00:55:58 +0600

    need to study more

---
+++ haskell/Smile.hs
@@ -0,0 +1,28 @@
+module Smile where
+
+import Control.Monad (liftM2)
+countListComp :: Eq a => [a] -> a -> Int
+countListComp [] find = 0
+countListComp ys find = length xs
+    where xs = [xs | xs <- ys, xs == find]
+
+validEyes :: Char -> Bool
+validEyes x = x `elem` ":;"
+
+validSmile :: Char -> Bool
+validSmile x  = x `elem` ")D"
+
+validNose :: Char  -> Bool
+validNose x = x `elem` "-~"
+
+countSmily :: String -> Bool
+countSmily "" = False 
+countSmily [x] = False
+countSmily [x, xs] = validEyes x && validSmile xs
+countSmily (x:xs:rest)
+    | validEyes x && validNose xs && validSmile (head rest) = True
+    | otherwise = False
+
+-- countSmileys :: [String] -> Int
+countSmileys :: [String] -> Int
+countSmileys = length . filter countSmily
